167fb34a0b397b3fc7cfe8e1479009eded7f8032
DeepakSingh260 <alexmha260@gmail.com>
2022-09-04 17:07:08 +0530

    designing book page

--- app/src/main/java/com/example/saathi/Book_Page.kt
+++ app/src/main/java/com/example/saathi/Book_Page.kt
@@ -1,11 +1,23 @@
 package com.example.saathi
 
+import android.media.Image
+import android.media.Rating
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridView
 
 class Book_Page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_page)
+
+        val gridView = findViewById<GridView>(R.id.main_grid_view)
+        val itemsList: MutableList<book> = ArrayList()
     }
 }
+
+data class book(
+    var name:String ,
+    var img:String ,
+    var rating: Rating
+)
