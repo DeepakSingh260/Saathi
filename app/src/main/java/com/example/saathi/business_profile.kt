9dc6a88b23f361fa78b2a6b0ffc4b8b7db0e8159
DeepakSingh260 <alexmha260@gmail.com>
2022-09-07 22:23:54 +0530

    designed business_profile.kt

---
+++ app/src/main/java/com/example/saathi/business_profile.kt
@@ -0,0 +1,11 @@
+package com.example.saathi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class business_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_business_login)
+    }
+}
